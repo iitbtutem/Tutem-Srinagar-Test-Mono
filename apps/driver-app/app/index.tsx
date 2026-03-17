054248acd5d9c7cdd7ea793e46d89b7c1f80dadb
zzZehran <zzzehran@gmail.com>
2026-03-17 14:15:01 +0530

    renamed root index.tsx of driver app

--- apps/driver-app/app/index.tsx
+++ apps/driver-app/app/index.tsx
@@ -23,7 +23,7 @@ const IMAGE_STYLE: ImageStyle = {
   width: 76,
 };
 
-export default function Screen() {
+export default function RootScreen() {
   const { colorScheme } = useColorScheme();
   return <Redirect href={'/(auth)/signin'} />;
   
